c41e51d1ce00991bed709b7a384ff9954d0147af
TiffanyJiayue_L <148782552+TiffanyJiayue@users.noreply.github.com>
2025-02-04 10:58:08 -0800

    Update employee_attrition_analysis.py

--- employee_attrition_analysis.py
+++ employee_attrition_analysis.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Dec 11 15:19:48 2024
-@author: Tiffany
-"""
-
 # Business scenario:
 # A large software company's HR department has hired you to better understand 
 # why some employees are more likely to leave the firm than others. 
@@ -27,11 +20,49 @@ from sklearn.metrics import mean_squared_error, confusion_matrix, accuracy_score
 from sklearn.neural_network import MLPClassifier
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.preprocessing import LabelEncoder
+import os
+os.environ["SQLALCHEMY_SILENCE_UBER_WARNING"] = '1'
+from sqlalchemy import create_engine, text
+import pymysql
+import pandas as pd
+MySQL_connection = 'XXXXXX'
 
 # Load the dataset
-path = '/Users/MachineLearning'
+path = '/Users/XXX/ECON2824_machine/Finalweek'
 data = pd.read_csv(path + '/Simulate_HRemployee_attrition.csv')
 
+# NOTE: I'm practicing Python and SQL at the same time.
+
+# Create a connection to the MySQL database using SQLAlchemy
+engine = create_engine(MySQL_connection)
+
+"""
+# (Commented-out code for reference)
+# Store the DataFrame `data` into the MySQL table `Simulate_HRemployee_attrition`
+# If the table already exists, replace it
+# The index of the DataFrame will be stored in the table
+data.to_sql(
+    name="Simulate_HRemployee_attrition",  
+    con=engine,               
+    if_exists="replace",        
+    index=True,               
+)
+"""
+
+# Define a SQL query to retrieve all data from the table `Simulate_HRemployee_attrition`
+query = "SELECT * FROM Simulate_HRemployee_attrition"
+
+try:
+    # Execute the SQL query and load the result into a pandas DataFrame
+    data = pd.read_sql(query, engine)
+    
+    # Print the first five rows of the retrieved data
+    print(data.head())  
+except Exception as e:
+    # Handle and print any errors that occur during the query execution
+    print(f"Error: {e}")
+
+# MACHINE LEARNING PART START
 # Data cleaning and preprocessing
 data = data.drop(['Over18', 'EmployeeCount', 'EmployeeNumber', 'StandardHours'], axis=1)
 data['Attrition'] = data['Attrition'].replace({'Yes': 1, 'No': 0})
@@ -109,4 +140,27 @@ gb_model = GradientBoostingRegressor(n_estimators=1000, learning_rate=0.01, rand
 gb_model.fit(X_train, y_train)
 gb_y_pred = gb_model.predict(X_test)
 print('Gradient Boosting RMSE:', np.sqrt(mean_squared_error(y_test, gb_y_pred)))
+
+
+# PRACTICE SQL AGAIN
+select_case='sqlalchemy_pandas'
+
+if select_case == 'sqlalchemy_pandas':  
+    print(select_case)
+    
+    # Create a connection to the MySQL database using SQLAlchemy
+    engine = create_engine(MySQL_connection)
+   
+    # Store the DataFrame `importance_df` into the MySQL table `feature_importance`
+    # If the table already exists, replace it
+    # The index of the DataFrame will be stored in the table
+    importance_df.to_sql(
+        name="feature_importance",  # Name of the target table
+        con=engine,                 # Database connection engine
+        if_exists="replace",        # Replace table if it already exists
+        index=True,                 # Include DataFrame index as a column
+    )
+
+
+
 
